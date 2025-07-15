31b0e89e83c6e1498243b6dd43927aff5e7eff95
Ishaan11042005 <ishaanmittal.bt23ece@pec.edu.in>
2025-07-15 23:21:50 +0530

    Update supabase-client.ts

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseURL = "https://gzjuzdfcsyxxakhiylkp.supabase.co";
+const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6anV6ZGZjc3l4eGFraGl5bGtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxNTI5NjQsImV4cCI6MjA2NzcyODk2NH0.xsSV3QdlATLDazW4lW2-MQyZgr7CZzV63kCMz_HppK0";
 
 export const supabase = createClient(supabaseURL, supabaseAnonKey);
